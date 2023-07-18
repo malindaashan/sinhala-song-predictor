d607387cf78ee3da3d90c82296ac38bd10a6e0e5
Malinda Pieris <malinda.p@aeturnum.com>
2023-07-18 10:03:08 +0530

    Initial commit sinhala song predictor backend

---
+++ sinhala-song-predictor-backend/sinhala-song-predictor-backend/src/test/java/com/msc/sinhalasongpredictorbackend/SinhalaSongPredictorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msc.sinhalasongpredictorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinhalaSongPredictorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
