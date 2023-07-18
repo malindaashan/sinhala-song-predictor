50153747b4c6e399c7127eca5e4d1b061a04740b
Malinda Pieris <malinda.p@aeturnum.com>
2023-07-18 10:20:54 +0530

    backend intial commit

---
+++ sinhala-song-predictor-backend/src/main/java/com/msc/sinhalasongpredictorbackend/SinhalaSongPredictorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.msc.sinhalasongpredictorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinhalaSongPredictorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinhalaSongPredictorBackendApplication.class, args);
+	}
+
+}
