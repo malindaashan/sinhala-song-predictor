af5c36f61ae736b7714ce4d2231d396fc47baeee
Malinda Pieris <malinda.p@aeturnum.com>
2023-09-11 09:09:16 +0530

    test commit github action

--- sinhala-song-predictor-backend/src/main/java/com/msc/sinhalasongpredictorbackend/SinhalaSongPredictorBackendApplication.java
+++ sinhala-song-predictor-backend/src/main/java/com/msc/sinhalasongpredictorbackend/SinhalaSongPredictorBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SinhalaSongPredictorBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SinhalaSongPredictorBackendApplication.class, args);
 	}
